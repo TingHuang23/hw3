703d2fc4f48bc3880c210951446d3f2baba072fe
Ting Huang <ting.huang@kellogg.northwestern.edu>
2023-05-19 05:35:33 +0000

    05190035

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
-Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+# Rails.application.routes.draw do
+#   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # get("/", { :controller => "articles", :action => "index" })
+#   # Defines the root path route ("/")
+#   # get("/", { :controller => "articles", :action => "index" })
+# end
+
+Rails.application.routes.draw do
+  # Define routes for places resource
+  resources :places
+
+  # Define routes for posts resource
+  resources :posts, only: [:index, :new, :create]
 end
